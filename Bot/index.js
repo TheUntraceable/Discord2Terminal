32bc83faba1ffd6223dd29f7876cb6e0ba8d8f1d
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-09-01 15:28:49 +0100

    Add Express routes

--- Bot/index.js
+++ Bot/index.js
@@ -1,18 +1,25 @@
-const { Client, IntentsBitField, InteractionType, PermissionsBitField } = require("discord.js")
+const { Client, IntentsBitField, InteractionType, PermissionsBitField, Routes, SlashCommandBuilder } = require("discord.js")
 const { MongoClient } = require('mongodb');
+const WebhookManager = require("./webhookClient")
+const express = require("express")
 const config = require("../config.json");
 
 const client = new Client({intents: [IntentsBitField.Flags.Guilds]});
+const app = express()
+app.use(express.json())
 
 client.mongo = new MongoClient(config.mongoUri, { useNewUrlParser: true });
 client.db = client.mongo.db("DiscordTerminal")
 client.db.webhooks = client.db.collection("webhooks");
 client.mongo.connect();
+client.webhookManagers = {}
+
 
 const generateDatabaseEntry = async interaction => {
     if(!await interaction.client.db.webhooks.findOne({guildId: interaction.guild.id})) {
         await interaction.client.db.webhooks.insertOne({
             guildId: interaction.guild.id,
+            channelId: interaction.channel.id,
             webhooks: []
         });
     }
@@ -36,7 +43,7 @@ client.on("interactionCreate", async (interaction) => {
 
                 while(amount >= created) {
 
-                    const webhook = await interaction.guild.createWebhook({reason: `Requested by ${interaction.member.tag} (${interaction.member.id})`})
+                    const webhook = await interaction.guild.createWebhook({reason: `Requested by ${interaction.member.tag} (${interaction.member.id})`, name: `Discord2Terminal`})
 
                     const payload = {
                         webhookId: webhook.id,
@@ -46,9 +53,9 @@ client.on("interactionCreate", async (interaction) => {
 
                     webhooks.push(payload)
                 } 
-                const existing = await interaction.client.db.webhooks.findOne({guildId: interaction.guild.id})
+                const existing = await interaction.client.db.webhooks.findOne({channelId: interaction.channel.id})
                 existing.webhooks.push(...webhooks)
-                await interaction.client.db.webhooks.updateOne({guildId: interaction.guild.id}, {$set: {webhooks: existing.webhooks}})
+                await interaction.client.db.webhooks.updateOne({channelId: interaction.channel.id}, {$set: {webhooks: existing.webhooks}})
                 await interaction.reply({content: `Created ${amount} webhooks!`})                                
             } else if(subcommand == "list") {
                 const existing = await interaction.client.db.webhooks.findOne({guildId: interaction.guild.id})
@@ -62,9 +69,65 @@ client.on("interactionCreate", async (interaction) => {
                 existing.webhooks.splice(0, amount)
                 await interaction.client.db.webhooks.updateOne({guildId: interaction.guild.id}, {$set: {webhooks: existing.webhooks}})
                 await interaction.reply({content: `Deleted ${amount} webhooks!`})
+            } else if(subcommand == "recommended") {
+                const active = interaction.options.getInteger("active-users")
+                return await interaction.reply({content: `I would recommend having ${active / 5} webhooks for ${active} active users!`}) // 5 users per webhook allows for 1 message from each user
             }
         }
     }
 })
+
+client.on("ready", async () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+    await client.rest.put(Routes.applicationCommands(client.id), {
+        body: [
+            new SlashCommandBuilder()
+                .setName("webhook")
+                .setDescription("Set up webhooks for this server for me to use!")
+                .setDMPermission(false)
+                .addSubcommand(option => 
+                    option.setName("create")
+                    .setDescription("Create webhooks for me to use!")
+                    .addIntegerOption(option =>
+                        option.setName("amount")
+                        .setDescription("The amount of webhooks to create")
+                    )
+                )
+                .addSubcommand(option => 
+                    option.setName("list")
+                    .setDescription("List the amount of webhooks I am using from this server")
+                )
+                .addSubcommand(option =>
+                    option.setName("delete")
+                    .setDescription("Delete webhooks from this server")
+                    .addIntegerOption(option =>
+                        option.setName("amount")
+                        .setDescription("The amount of webhooks to delete")
+                    )
+                ).addSubcommand(option => 
+                    option.setName("recommended")
+                    .setDescription("Get the recommended amount of webhooks to create")
+                    .addIntegerOption(option =>
+                        option.setName("active-users")
+                        .setDescription("The amount of active users in this server")
+                    )
+                )
+        ]
+    })
+})
+
+app.post("/channels/:channelId", async (req, res) => {
+
+    const dbEntry = await client.db.webhooks.findOne({channelId: req.params.channelId})
+    
+    if(!dbEntry || !dbEntry.webhooks) return res.status(404).json({
+        error: "No webhooks found for this channel!"
+    })
+    const manager = client.webhookManagers[req.params.channelId] || new WebhookManager(dbEntry.webhooks)
+    client.webhookManagers[req.params.channelId] = manager
+    const data = await manager.execute(await manager.findAvailableWebhook(), req.body.message, {username: req.body.username, avatar: req.body.avatar_url})
+    res.status(200).json(data)
+})
+
 
 client.login(config.clientToken)
---
+++ Bot/webhookClient.js
@@ -0,0 +1,80 @@
+const axios = require("axios")
+const chalk = require("chalk")
+
+class Webhook {
+    constructor(webhookId, webhookToken) {
+        this.webhookId = webhookId
+        this.webhookToken = webhookToken
+        this.url = `https://discord.com/api/v10/webhooks/${webhookId}/${webhookToken}`
+    }
+}
+
+class RatelimitedWebhook extends Webhook {
+    constructor(webhookId, webhookToken, expireIn) {
+        super(webhookId, webhookToken)
+        this.expireIn = expireIn
+    }
+}
+
+module.exports = class WebhookClient {
+    constructor(webhooks) {
+        this.webhooks = webhooks // A list of Webhook
+        this.ratelimited = [] // A list of RatelimitedWebhook
+    }
+
+    async findAvailableWebhook() {
+        if(this.webhooks[0]) {
+            console.log(chalk.green.underline("Found available webhook!"))
+            return this.webhooks[0]
+        } else { // If we don't have any available webhooks
+            console.log(chalk.yellow.underline("No available webhooks! Waiting for the shortest webhook to reset..."))
+            var shortest;
+            for(const webhook of this.ratelimited) {
+                if(!shortest || shortest.expireIn > webhook.expireIn) {
+                    shortest = webhook
+                }
+                console.log(chalk.yellow(`Webhook ${webhook.webhookId} is on cooldown for ${webhook.expireIn} seconds, waiting...`))
+                await sleep(shortest.expireIn + 1)
+                return shortest
+            }
+        }
+    }
+
+    async execute(webhook, message, author) {
+        console.log(chalk.green.underline("Executing webhook..."))
+        const response = await axios({
+            method: "post",
+            url: webhook.url,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            data: {
+                content: message,
+                username: author.username,
+                avatar_url: author.avatar
+            }
+        })
+
+        if(response.status == 429) {
+            console.log(chalk.red.underline.bold(`Webhook ${webhook.webhookId} is ratelimited! Adding it to the cooldown list...`))
+            this.ratelimited.push(new RatelimitedWebhook(webhook.webhookId, webhook.webhookToken, response.data.retry_after))
+            setTimeout(() => {
+                console.log(chalk.yellow.underline(`Webhook ${webhook.webhookId} is no longer ratelimited!`))
+                this.ratelimited = this.ratelimited.filter(webhook => webhook.webhookId != webhook.webhookId)
+
+            })
+            const newWebhook = await this.findAvailableWebhook()
+            if(newWebhook) {
+                console.log(chalk.green.underline.bold(`Found available webhook! Retrying execution of webhook ${newWebhook.webhookId}...`))
+                return await this.execute(newWebhook, message, author)
+            }
+        } else if(response.headers["X-RateLimit-Remaining"] == 0) {
+            this.ratelimited.push(new RatelimitedWebhook(webhook.webhookId, webhook.webhookToken, response.headers["X-RateLimit-Reset-After"]))
+            setTimeout(() => {
+                this.ratelimited = this.ratelimited.filter(webhook => webhook.webhookId != webhook.webhookId)
+            }, response.headers["X-RateLimit-Reset-After"])
+        } else if(response.status == 204) {
+            return response.data
+        }
+    }
+}
