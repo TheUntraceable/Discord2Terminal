f70d1ca3be77cfe1fb6fb5fdeae7b44bb3afa5a6
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-09-03 19:51:56 +0100

    Make commands its own thing

---
+++ Client/commands/select.js
@@ -0,0 +1,39 @@
+export default {
+    name: "select",
+    callback: async (guildString = "", channelString = "") => {
+        const guilds = {}
+        for(const guild of client.guilds) {
+            guilds[guild.name] = guild
+        }
+        const answer = await inquirer.prompt({
+            type: "list",
+            name: "guild",
+            message: "Select a guild",
+            choices: Object.keys(guilds).filter(guild => guild.toLowerCase().includes(guildString.toLowerCase()))
+        })
+        const guild = guilds[answer.guild]
+    
+        const _channels = await client.getChannels(guild.id)
+        const channels = _channels.filter(channel => channel.type == 0)
+        const channelNameToChannel = {}
+       
+        for(const channel of channels) {
+            channelNameToChannel[channel.name.toLowerCase()] = channel
+        }
+    
+        const channel = await inquirer.prompt({
+            type: "list",
+            name: "channel",
+            message: "Select a channel",
+            choices: channels.filter(channel => channel.type == 0 && channel.name.toLowerCase().includes(channelString.toLowerCase())).map(channel => channel.name)
+        })
+    
+        var message = ""
+        for(const cachedMessage of client.channels[String(channelNameToChannel[channel.channel].id)]) {
+            var messageBlock = chalk.hex(cachedMessage.author_color).underline(`${cachedMessage.author.username}#${cachedMessage.author.discriminator} (${cachedMessage.author.id})`)
+            messageBlock += `\n    ${chalk.hex(cachedMessage.author_color)(cachedMessage.content)}`
+            message += messageBlock
+        }
+        console.log(message)
+    }
+}
--- Client/index.js
+++ Client/index.js
@@ -5,22 +5,25 @@ import config from "../config.json" assert {type: "json"}
 import inquirer from "inquirer"
 import { createSpinner } from "nanospinner"
 import chalk from "chalk"
+import fs from "fs"
 
+marked.setOptions({
+    renderer: new TerminalRenderer( )
+});
 
 const client = new Client({
     transport: "ipc"
 })
+
 client.channels = {}
 client.commands = {}
 
 
 client.addCommand = (name, callback) => {
     client.commands[name] = callback
+    return client
 }
 
-marked.setOptions({
-    renderer: new TerminalRenderer( )
-});
 
 const parseCommands = async () => {
     const answer = await inquirer.prompt({
@@ -64,61 +67,27 @@ client.on("ready", async () => {
     await parseCommands()
 })
 
-client.addCommand("select", async (guildString = "", channelString = "") => {
-    const guilds = {}
-    for(const guild of client.guilds) {
-        guilds[guild.name] = guild
-    }
-    const answer = await inquirer.prompt({
-        type: "list",
-        name: "guild",
-        message: "Select a guild",
-        choices: Object.keys(guilds).filter(guild => guild.toLowerCase().includes(guildString.toLowerCase()))
-    })
-    const guild = guilds[answer.guild]
-
-    const _channels = await client.getChannels(guild.id)
-    const channels = _channels.filter(channel => channel.type == 0)
-    const channelNameToChannel = {}
-   
-    for(const channel of channels) {
-        channelNameToChannel[channel.name.toLowerCase()] = channel
-    }
-
-    const channel = await inquirer.prompt({
-        type: "list",
-        name: "channel",
-        message: "Select a channel",
-        choices: channels.filter(channel => channel.type == 0 && channel.name.toLowerCase().includes(channelString.toLowerCase())).map(channel => channel.name)
-    })
-
-    var message = ""
-    for(const cachedMessage of client.channels[String(channelNameToChannel[channel.channel].id)]) {
-        var messageBlock = chalk.hex(cachedMessage.author_color).underline(`${cachedMessage.author.username}#${cachedMessage.author.discriminator} (${cachedMessage.author.id})`)
-        messageBlock += `\n    ${chalk.hex(cachedMessage.author_color)(cachedMessage.content)}`
-        message += messageBlock
-    }
-    console.log(message)
-    
-})
-
-client.addCommand("cls", async () => {
-    console.clear()
-})
-
-client.addCommand("clear", async () => {
-    console.clear()
-})
-
-client.addCommand("eval", async (code = "") => {
-    await eval(code)
-})
-
 client.on("MESSAGE_CREATE", async payload => {
     payload.message.content = marked(payload.message.content) 
     client.channels[payload.channel_id].push(payload.message)
 })
 
+client.addCommand("cls", async () => {
+    console.clear()
+}).addCommand("eval", async (code = "") => {
+    await eval(code)
+}).addCommand("clear", async () => {
+    console.clear()
+})
+
+for(const file of fs.readdirSync("./commands")) {
+    const spinner = createSpinner(`Loading command ${file}...`).start()
+    const command = await import(`./commands/${file}`)
+    client.addCommand(command.name, command.callback)
+    spinner.success({
+        text: `Loaded command ${file}!`
+    })
+}
 
 client.login({
     clientId: config.clientId,
