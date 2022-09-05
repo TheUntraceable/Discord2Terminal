dc21d3a8ec4454122d925eb658606cbfbab68ffa
TheUntraceable <TheUntraceable@TheUntraceable.tech>
2022-09-05 17:16:41 +0100

    Current state

--- Client/events/messageCreate.js
+++ Client/events/messageCreate.js
@@ -8,6 +8,7 @@ marked.setOptions({
 export const data = {
     name: "MESSAGE_CREATE",
     async callback(payload) {
+        
         payload.message.content = marked(payload.message.content) 
         payload.client.channels[payload.channel_id].push(payload.message)
     }
