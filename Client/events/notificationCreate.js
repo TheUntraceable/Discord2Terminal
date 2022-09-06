471ad67df8f507d9c648162e0363984fc9774c14
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-09-06 19:43:23 +0100

    Fix author displaying logics

--- Client/events/notificationCreate.js
+++ Client/events/notificationCreate.js
@@ -15,11 +15,13 @@ export const data = {
             }
             pushed = true
         }
+
+        if(!pushed) channel.created.push(message)
+
         const channel = payload.client.channels[message.channel_id]
         if(channel.created.slice(-1)[0].author.id != message.author.id || channel.created.length == 1) {
             console.log("\n" + chalk.hex(message.author_color || "#FFFFFF11").underline(`${message.author.username}#${message.author.discriminator} (${message.author.id})`))
         }
         console.log(`\n  ${chalk.hex(message.author_color || "#FFFFFF11")(message.content)}`)
-        if(!pushed) channel.created.push(message)
     }
 }
