e12ff6cf451af24c84e6aa46e414a0ed53e81751
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-09-06 19:55:32 +0100

    Add logics for messageUpdate

--- Client/events/messageUpdate.js
+++ Client/events/messageUpdate.js
@@ -1,6 +1,8 @@
 export const data = {
     name: "MESSAGE_UPDATE",
     async callback(payload) {
-        // SOOOOOOOOOOOOOON
-    }
+        const channel = payload.client.channels[payload.channel_id]
+        if(!channel) return
+        channel.updated.push(payload.message)
+   }
 }
