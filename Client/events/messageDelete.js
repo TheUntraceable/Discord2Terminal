6bcc870f210dce81ba22af3be3f6688288f48412
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-09-06 19:50:24 +0100

    Add logics for messageDelete

--- Client/events/messageDelete.js
+++ Client/events/messageDelete.js
@@ -1,6 +1,8 @@
 export const data = {
     name: "MESSAGE_DELETE",
     async callback(payload) {
-        // SoonTM
+        const channel = payload.client.channels[payload.channel_id]
+        if(!channel) return
+        channel.deleted.push(payload.message)
     }
 }
