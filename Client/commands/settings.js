adf922d8ab6c899ba861af79d1867f01a6d99046
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-09-10 13:57:16 +0100

    Save settings to file

--- Client/commands/settings.js
+++ Client/commands/settings.js
@@ -1,4 +1,5 @@
 import chalk from "chalk"
+import fs from "fs"
 
 export const data = {
     name: "settings",
@@ -13,6 +14,7 @@ export const data = {
                 client.settings[key] = value
                 console.log(chalk.green(`${chalk.bold(key)}: ${value}`))
             }
+            fs.writeFile("../settings.json", JSON.stringify(client.settings, null, 4), err => console.log(`Failed to write settings with error: ${err}`))
         }
     }
 }
