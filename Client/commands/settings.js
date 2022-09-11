4659da62a2e083a447bdacc6216ef96dd6ca7c40
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-09-11 15:04:41 +0100

    Use flags for settings command

--- Client/commands/settings.js
+++ Client/commands/settings.js
@@ -3,18 +3,26 @@ import fs from "fs"
 
 export const data = {
     name: "settings",
-    async callback(client, flags, subcommand) {
+    async callback(client, subcommand, ...args) {
         if(subcommand == "list") {
             for(const setting of Object.keys(client.settings)) {
-                console.log(`${chalk.bold(setting)}: ${client.settings[setting]}`)
+                console.log(`${chalk.green(chalk.bold(setting))}: ${JSON.stringify(client.settings[setting])}`)
             }
         } else if(subcommand == "set") {
-            for([key, value] of Object.entries(flags)) {
+
+            const flags = {}
+            for(const arg of args) {
+                if(arg.startsWith("--")) {
+                    flags[arg.slice(2)] = args.indexOf(arg) + 1
+                }
+            }
+
+            for(const [key, value] of Object.entries(flags)) {
                 if(!["ignoreUsers", "ignoreGuilds", "ignoreBlocked", "ignoreChannels", "compact"].includes(key)) continue
                 client.settings[key] = value
                 console.log(chalk.green(`${chalk.bold(key)}: ${value}`))
             }
-            fs.writeFile("../settings.json", JSON.stringify(client.settings, null, 4), err => console.log(`Failed to write settings with error: ${err}`))
+            fs.writeFile("./settings.json", JSON.stringify(client.settings, null, 4), err => {if(err) console.log(`Failed to write settings with error: ${err}`)})
         }
     }
 }
