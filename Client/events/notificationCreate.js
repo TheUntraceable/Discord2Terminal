8c95d38c0bc306f6703e65630813b531b11bb89b
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-09-11 16:00:10 +0100

    Remove redundant code

--- Client/events/notificationCreate.js
+++ Client/events/notificationCreate.js
@@ -1,15 +1,15 @@
 import chalk from "chalk"
+import parseMentions from "../utils/parseMentions"
 
 export const data = {
     name: "NOTIFICATION_CREATE",
     callback: async (payload) => {
         if(payload.message.author.system) return
         const message = payload.message
-        let pushed = false
 
         if(!payload.client.channels[message.channel_id]) {
             payload.client.channels[message.channel_id] = {
-                created: [message],
+                created: [],
                 updated: [],
                 deleted: []
             }
@@ -18,7 +18,9 @@ export const data = {
 
         const channel = payload.client.channels[message.channel_id]
 
-        if(!pushed) channel.created.push(message)
+        await parseMentions(payload)
+
+        channel.created.push(message)
 
         if(channel.created.slice(-1)[0].author.id != message.author.id || channel.created.length == 1) {
             console.log("\n" + chalk.hex(message.author_color || "#FFFFFF11").underline(`${message.author.username}#${message.author.discriminator} (${message.author.id})`))
