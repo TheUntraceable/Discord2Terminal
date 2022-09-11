6e98ccb3261ab0f0bb810ae2608600bc0aa09555
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-09-11 16:02:13 +0100

    Don't cache deleted messages we don't know the content of

--- Client/events/messageDelete.js
+++ Client/events/messageDelete.js
@@ -3,7 +3,7 @@ export const data = {
     async callback(payload) {
         const channel = payload.client.channels[payload.channel_id]
         if(!channel) return
+        if(!channel.created.find(message => message.id == payload.message.id)) return
         channel.deleted.push(payload.message)
-
     }
 }
