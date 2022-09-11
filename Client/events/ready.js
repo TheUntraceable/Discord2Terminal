26a1f4023eb328aae4bc7495c6f2da349a158db3
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-09-11 15:59:02 +0100

    Make ready more readable

--- Client/events/ready.js
+++ Client/events/ready.js
@@ -8,14 +8,15 @@ import chalk from "chalk"
 export const data = {
     name: "ready",
     async callback(payload) {
+
         payload.client.settings.token = {
             expiresAt: new Date(payload.expires),
             accessToken: payload.client.accessToken
         }
 
         await fs.writeFile("./settings.json", JSON.stringify(payload.client.settings, null, 4))
-
         payload.client.subscribe("NOTIFICATION_CREATE")
+        
         let subscribed = 0
         const guilds = await payload.client.getGuilds()
         payload.client.guilds = guilds.guilds
@@ -35,20 +36,24 @@ export const data = {
             text: `Cached ${Object.keys(payload.client.channels).length} channels!`
         })
         console.log(chalk.green.underline(`Subscribing to events to Discord... (takes <1m)`))
+
         const subscriptionBar = new ProgressBar("[:bar] :rate subscriptions per second :percent done :etas :current/:total channels", { 
             total: Object.keys(payload.client.channels).length,
             complete: chalk.green("="),
             incomplete: chalk.red("-"),
             width: 50
         })
+
         for(const channelId of Object.keys(payload.client.channels)) {
             await payload.client.subscribe("MESSAGE_CREATE", { channel_id: channelId })
             await payload.client.subscribe("MESSAGE_UPDATE", { channel_id: channelId })
             await payload.client.subscribe("MESSAGE_DELETE", { channel_id: channelId })
+
             subscriptionBar.tick()
             subscribed++
         }
         subscriptionBar.terminate()
+ 
         console.log(chalk.green.underline(`Subscribed to MESSAGE_CREATE/UPDATE/DELETE in ${subscribed} channels!`))
         await parseCommands(payload.client)
     }
