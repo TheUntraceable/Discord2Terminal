95d540281a048fe594a46f6d4925a86d4ba92722
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-09-11 17:01:10 +0100

    Clean up code

--- Client/commands/join.js
+++ Client/commands/join.js
@@ -1,35 +1,13 @@
-import inquirer from "inquirer"
 import chalk from "chalk"
+import { ChannelType } from "discord.js"
+import { getChannelFromName } from "../utils/getChannelFromName"
+import { getGuildFromName } from "../utils/getGuildFromName"
 
 export const data = {
     name: "join",
     async callback(client, guildString = "", channelString = "") {
-        const guilds = {}
-        for(const guild of client.guilds) {
-            guilds[guild.name] = guild
-        }
-        const answer = await inquirer.prompt({
-            type: "list",
-            name: "guild",
-            message: "Select a guild",
-            choices: Object.keys(guilds).filter(guild => guild.toLowerCase().includes(guildString.toLowerCase()))
-        })
-        const guild = guilds[answer.guild]
-    
-        const channels = (await client.getChannels(guild.id)).filter(channel => [2, 13].includes(channel.type))
-        const channelNameToChannel = {}
-       
-        for(const channel of channels) {
-            channelNameToChannel[channel.name.toLowerCase()] = channel
-        }
-    
-        const channel = await inquirer.prompt({
-            type: "list",
-            name: "channel",
-            message: "Select a channel",
-            choices: channels.filter(channel => channel.name.toLowerCase().includes(channelString.toLowerCase())).map(channel => channel.name)
-        })
-        const selectedChannel = channelNameToChannel[channel.channel.toLowerCase()]
+        const guild = await getGuildFromName(client, guildString)
+        const channel = await getChannelFromName(client, guild, channelString,-0 [ChannelType.GuildVoice, ChannelType.GuildStageVoice])
         try {
             await client.selectVoiceChannel(selectedChannel.id, {
                 force: true
