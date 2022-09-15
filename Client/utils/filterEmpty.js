76e895859579c1ceeca78cf4a19ce888c0128faa
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-09-15 18:26:46 +0100

    payload.client -> client

--- Client/utils/filterEmpty.js
+++ Client/utils/filterEmpty.js
@@ -3,6 +3,6 @@ export default async (client, channelId, channel) => {
     channel.updated.filter(Boolean)
     channel.deleted.filter(Boolean)
     await payload.client.channels.set(channelId, channel)
-    const newChannel = await payload.client.channels.get(channelId)
+    const newChannel = await client.channels.get(channelId)
     return newChannel
 }
