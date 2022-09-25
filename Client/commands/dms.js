bcd6153e59e475b99cb28c6acdb02528e6e8cb83
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-09-25 21:06:34 +0100

    Add dms command

--- Client/commands/dms.js
+++ Client/commands/dms.js
@@ -1,9 +1,22 @@
+import inquirer from "inquirer"
 export const data = {
     name: "dms",
-    async callback(client) {
+    async callback(client, username) {
         const users = await client.users.all()
-        for(const user of users.map(x => x.value)) {
-            console.log(user)
+        const validUsers = users.map(user => user.value).filter(user => user.username.toLowerCase().includes(username.toLowerCase()))
+        if(!validUsers) return console.log("User not found!")
+        const selectedUserAnswer = await inquirer.prompt({
+            type: "list",
+            name: "username",
+            message: "Select the user you want to view!",
+            choices: validUsers.map(user => user.username)
+        })
+        const selectedUser = validUsers.find(user => user.username == selectedUserAnswer.username)
+        if(!selectedUser) {
+            console.log("Something went wrong!")
+        }
+        for(const message of selectedUser.messages) {
+            console.log(`  ${message}\n`)
         }
     }
 }
