be35e849538ab5111a50ecf59590f6ba58e948e6
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-10-11 17:25:15 +0100

    Add snipe command

--- Client/commands/dms.js
+++ Client/commands/dms.js
@@ -1,17 +1,20 @@
+import chalk from "chalk"
 import inquirer from "inquirer"
 export const data = {
     name: "dms",
-    async callback(client, username) {
+    async callback(client, username = "") {
         const users = await client.users.all()
-        const validUsers = users.map(user => user.value).filter(user => user.username.toLowerCase().includes(username.toLowerCase()))
-        if(!validUsers) return console.log("User not found!")
+        const validUsers = users.map(user => user.value).filter(user => {
+            return user.user.username.toLowerCase().includes(username.toLowerCase())
+        })
+        if(!validUsers) return console.log(chalk.red("User not found!"))
         const selectedUserAnswer = await inquirer.prompt({
             type: "list",
             name: "username",
             message: "Select the user you want to view!",
-            choices: validUsers.map(user => user.username)
+            choices: validUsers.map(user => user.user.username)
         })
-        const selectedUser = validUsers.find(user => user.username == selectedUserAnswer.username)
+        const selectedUser = validUsers.find(user => user.user.username == selectedUserAnswer.username)
         if(!selectedUser) {
             console.log("Something went wrong!")
         }
--- Client/commands/snipe.js
+++ Client/commands/snipe.js
@@ -1,6 +1,34 @@
+import chalk from "chalk"
+import getChannelFromName from "../utils/getChannelFromName.js"
+import getGuildFromName from "../utils/getGuildFromName.js"
+
+
 export const data = {
     name: 'snipe',
-    async callback() {
-        
+    async callback(client, guildString = "", channelString = "") {
+        const guild = await getGuildFromName(client, guildString)
+        const channel = await getChannelFromName(client, guild, channelString)
+        const channelMessages = await client.channels.get(channel.id)
+
+        if(!channelMessages.deleted) {
+            console.log(chalk.red("No deleted messages in this channel!"))
+            return
+        }
+
+        let lastAuthor = null
+
+        for(const message of channelMessages.created) {
+            if(channelMessages.deleted.find(m => m.id == message.id)) {
+
+                if(lastAuthor != message.author.id) {
+                    console.log(chalk.hex(message.author_color || "#FFFFFF11").underline(message.author.username))
+                }
+
+                console.log(chalk.red(`  ${message.content}`))
+                lastAuthor = message.author.id
+            }
+        }
+
     }
+
 }
