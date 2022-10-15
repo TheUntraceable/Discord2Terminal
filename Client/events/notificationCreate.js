0952e3a8be607e5077150e5379a66f6a8cba0716
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-10-15 14:24:28 +0100

    Use channel_id

--- Client/events/notificationCreate.js
+++ Client/events/notificationCreate.js
@@ -11,17 +11,17 @@ export const data = {
         if(payload.client.settings.ignoreBlocked && payload.message.author.blocked) return
         const message = payload.message
 
-        if(!await payload.client.channels.has(String(message.channel_id))) {
+        if(!await payload.client.channels.has(String(payload.channel_id))) {
             try {
-                const channel = await payload.client.getChannel(message.channel_id)
-                await payload.client.channels.set(message.channel_id, {
+                const channel = await payload.client.getChannel(payload.channel_id)
+                await payload.client.channels.set(payload.channel_id, {
                     name: channel.name,
                     created: [],
                     updated: [],
                     deleted: []
                 })
             } catch {
-                await payload.client.channels.set(message.channel_id, {
+                await payload.client.channels.set(payload.channel_id, {
                     created: [],
                     updated: [],
                     deleted: []
@@ -29,12 +29,12 @@ export const data = {
             }
         }
 
-        const channel = await payload.client.channels.get(message.channel_id)
+        const channel = await payload.client.channels.get(payload.channel_id)
         await filterEmpty(payload.client, payload.channel_id, channel)
 
         await parseMentions(payload)
 
-        await payload.client.channels.push(`${message.channel_id}.created`, message)
+        await payload.client.channels.push(`${payload.channel_id}.created`, message)
 
         if(channel.created.slice(-1)[0].author.id != message.author.id || channel.created.length == 1) {
             console.log("\n" + chalk.hex(message.author_color || "#FFFFFF11").underline(`${message.author.username}#${message.author.discriminator} (${message.author.id}) (${chalk.blue("NOTIFICATION")})`))
